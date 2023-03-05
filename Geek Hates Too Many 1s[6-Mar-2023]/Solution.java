b15386a6e13d817143757aebe450c02e95fbcb05
Bugs In Code <125942331+BugsInCodeYT@users.noreply.github.com>
2023-03-06 00:37:46 +0530

    Update Solution.java

--- Geek Hates Too Many 1s[6-Mar-2023]/Solution.java
+++ Geek Hates Too Many 1s[6-Mar-2023]/Solution.java
@@ -2,14 +2,17 @@
 class Solution {
     public static int noConseBits(int n) {
         
+        
         int[] bits=new int[32];
         
+        //Storing the bits of the given number
         for(int i=0;i<32;i++)
         {
             int bit=((1<<i)&n)>0?1:0;
             bits[31-i]=bit;
         }
         
+        //sliding window approach (we'll make rightmost bit zero for each 3 consecutive 1s).
         int i=0,j=0;
         int sum=0;
         while(j<32)
@@ -37,6 +40,7 @@ class Solution {
         }
         
         
+        //forming number from the bits array
         int num=0;
         
         for(i=31;i>=0;i--)

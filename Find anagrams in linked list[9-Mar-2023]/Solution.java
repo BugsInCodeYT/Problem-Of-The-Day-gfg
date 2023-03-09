6deed9185be02328c27de481bc48942479f1f0d7
Bugs In Code <125942331+BugsInCodeYT@users.noreply.github.com>
2023-03-09 10:53:57 +0530

    Update Solution.java

--- Find anagrams in linked list[9-Mar-2023]/Solution.java
+++ Find anagrams in linked list[9-Mar-2023]/Solution.java
@@ -1,52 +1,59 @@
 class Solution {
+    public static boolean areEqual(int[] a,int[] b)
+    {
+        for(int i=0;i<26;i++)
+        {
+            if(a[i]!=b[i]) return false;
+        }
+        
+            return true;
+    }
     public static ArrayList<Node> findAnagrams(Node head, String s) {
         
         ArrayList<Node> ans=new ArrayList<>();
         
+        Node st=head,end=head;
+        int i=0,j=0;
         int[] map=new int[26];
-        int[] listMap=new int[26];
-        
+        int[] curMap=new int[26];
         for(char ch:s.toCharArray())
         {
-            map[ch-'a']++;   
+            map[ch-'a']++;
         }
         
-        //find the length of map
-        int length=0;
-        for(int i=0;i<26;i++) if(map[i]>0) length++;
-        
-        //two pointer start and end.
-        Node st=head,end=head;
-        
-        //to keep track of count of character which have equal frequency in given string and list traversed till a particular index.
-        int cnt=0;
         while(end!=null)
         {
-            listMap[end.data-'a']++;
-            
-            if(map[end.data-'a']==listMap[end.data-'a']) cnt++;
-            
-            
-            while(map[end.data-'a']<listMap[end.data-'a'])
+            if(j<s.length()-1)
             {
-                if(map[st.data-'a']==listMap[st.data-'a']) cnt--;
-                
-                listMap[st.data-'a']--;
-                st=st.next;
-            }
-            
-            if(cnt==length)
-            {
-                ans.add(st);
-                Node next=end.next;
-                end.next=null;
-                st=next;
-                end=next;
-                cnt=0;
-                Arrays.fill(listMap,0);
+                curMap[end.data-'a']++;
+                j++;
+                end=end.next;
             }
             else
-                end=end.next;
+            {
+                curMap[end.data-'a']++;
+                j++;
+                
+                if(areEqual(map,curMap))
+                {
+                    ans.add(st);
+                    Node next=end.next;
+                    end.next=null;
+                    
+                    i=0; j=0;
+                    
+                    st=next; end=next;
+                    
+                    Arrays.fill(curMap,0);
+                }
+                else
+                {
+                    curMap[st.data-'a']--;
+                    i++;
+                    st=st.next;
+                    end=end.next;
+                }
+            }
         }
         
             return ans;

5a1935d455cee46ade02a210d6efafdc24e64d0f
Bugs In Code <125942331+BugsInCodeYT@users.noreply.github.com>
2023-03-09 10:53:24 +0530

    Update Solution.cpp

--- Find anagrams in linked list[9-Mar-2023]/Solution.cpp
+++ Find anagrams in linked list[9-Mar-2023]/Solution.cpp
@@ -1,57 +1,58 @@
-class Solution {
-  public:
-  vector<Node*> findAnagrams(Node* head,string s) {
-    vector<Node*> ans;
-    vector<int> map(26, 0);
-    vector<int> listMap(26, 0);
-
-   
-    for(char ch : s) {
-        map[ch - 'a']++;
-    }
-
-    int length = 0;
-    for(int i=0; i<26; i++) {
-        if(map[i] > 0) {
-            length++;
-        }
-    }
-
-    Node* st = head;
-    Node* end = head;
-    int cnt = 0;
-
-    while(end != nullptr) {
-        listMap[end->data - 'a']++;
-
-        if(map[end->data - 'a'] == listMap[end->data - 'a']) {
-            cnt++;
-        }
-
-        while(map[end->data - 'a'] < listMap[end->data - 'a']) {
-            if(map[st->data - 'a'] == listMap[st->data - 'a']) {
-                cnt--;
+class Solution{
+public:
+    bool areEqual(vector<int>& a, vector<int>& b) {
+        for(int i=0; i<26; i++) {
+            if(a[i] != b[i]) {
+                return false;
             }
-
-            listMap[st->data - 'a']--;
-            st = st->next;
         }
-
-        if(cnt == length) {
-            ans.push_back(st);
-            Node* next = end->next;
-            end->next = nullptr;
-            st = next;
-            end = next;
-            cnt = 0;
-            fill(listMap.begin(), listMap.end(), 0);
-        }
-        else {
-            end = end->next;
-        }
+    
+        return true;
     }
-
-    return ans;
-}
-
+    vector<Node*> findAnagrams(Node* head, string s) {
+        vector<Node*> ans;
+        Node* st = head;
+        Node* end = head;
+        int i = 0, j = 0;
+        vector<int> map(26, 0);
+        vector<int> curMap(26, 0);
+    
+        for(char ch : s) {
+            map[ch - 'a']++;
+        }
+    
+        while(end != nullptr) {
+            if(j < s.length() - 1) {
+                curMap[end->data - 'a']++;
+                j++;
+                end = end->next;
+            }
+            else {
+                curMap[end->data - 'a']++;
+                j++;
+    
+                if(areEqual(map, curMap)) {
+                    ans.push_back(st);
+                    Node* next = end->next;
+                    end->next = nullptr;
+    
+                    i = 0;
+                    j = 0;
+    
+                    st = next;
+                    end = next;
+    
+                    fill(curMap.begin(), curMap.end(), 0);
+                }
+                else {
+                    curMap[st->data - 'a']--;
+                    i++;
+                    st = st->next;
+                    end = end->next;
+                }
+            }
+        }
+    
+        return ans;
+    }
 };

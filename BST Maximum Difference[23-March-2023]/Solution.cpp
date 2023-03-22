7d8c2e0dfa84de11bcf42ccd45ea221de8e3e3bf
Bugs In Code <125942331+BugsInCodeYT@users.noreply.github.com>
2023-03-23 01:39:01 +0530

    Update Solution.cpp

--- BST Maximum Difference[23-March-2023]/Solution.cpp
+++ BST Maximum Difference[23-March-2023]/Solution.cpp
@@ -3,42 +3,38 @@
 
 class Solution {
 public:
-    static int find(Node *root, int target, Node* &targetNode) {
+    int find(Node* root, int target, Node** targetNode) {
         if (root == NULL) return INT_MIN;
         if (root->data == target) {
-            targetNode = root;
+            *targetNode = root;
             return target;
         }
-        int left = find(root->left, target, targetNode);
-        int right = find(root->right, target, targetNode);
-
-        if (left == INT_MIN) {
-            if (right == INT_MIN) return INT_MIN;
-            else return root->data + right;
-        }
-        else {
-            return root->data + left;
+        
+        if (target > root->data) {
+            return root->data + find(root->right, target, targetNode);
+        } else {
+            return root->data + find(root->left, target, targetNode);
         }
     }
-
-    static int solve(Node *root) {
+    
+    int solve(Node* root) {
         if (root == NULL) return INT_MAX / 2;
         if (root->left == NULL && root->right == NULL) return root->data;
-
+        
         int left = root->data + solve(root->left);
         int right = root->data + solve(root->right);
-
+        
         return min(left, right);
     }
-
-    static int maxDifferenceBST(Node *root, int target) {
+    
+    int maxDifferenceBST(Node* root, int target) {
         Node* targetNode = NULL;
-        int rootToTarget = find(root, target, targetNode);
-
+        int rootToTarget = find(root, target, &targetNode);
+        
         if (targetNode == NULL) return -1;
-
+        
         int targetToLeaf = solve(targetNode);
-
+        
         return rootToTarget - targetToLeaf;
     }
 };

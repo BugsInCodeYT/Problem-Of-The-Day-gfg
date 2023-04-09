5f0e709c53969de4e1163b4997001cdd813fa672
Bugs In Code <125942331+BugsInCodeYT@users.noreply.github.com>
2023-04-09 19:19:13 +0530

    Update Solution.py

--- Special Digits [ 09 - April - 2023]/Solution.py
+++ Special Digits [ 09 - April - 2023]/Solution.py
@@ -1,14 +1,11 @@
-#   >> PYTHON CODE <<
-
 mod = int(1e9) + 7
-
 def check(num, C, D):
     while num > 0:
         val = num % 10
-        if val != C and val != D:
-            return False
+        if val == C or val == D:
+            return True
         num //= 10
-    return True
+    return False
 
 def binPower(a, n):
     res = 1
@@ -18,16 +15,24 @@ def binPower(a, n):
         a = (a * a) % mod
         n //= 2
     return res
+    
+class Solution:
+    def bestNumbers(self, N : int, A : int, B : int, C : int, D : int) -> int:
 
-def bestNumbers(N, A, B, C, D):
-    fact = [0] * (N + 1)
-    fact[0] = 1
-    for i in range(1, N + 1):
-        fact[i] = (fact[i - 1] * i) % mod
-    ans = 0
-    for i in range(N + 1):
-        sum = i * A + (N - i) * B
-        if check(sum, C, D):
-            ans += fact[N] * binPower((fact[i] * fact[N - i]) % mod, mod - 2)
-            ans %= mod
-    return int(ans)
+        if A == B:
+            if check(N * A, C, D):
+                return 1
+            return 0
+        
+        fact = [0] * (N + 1)
+        fact[0] = 1
+        for i in range(1, N + 1):
+            fact[i] = (fact[i - 1] * i) % mod
+        ans = 0
+        for i in range(N + 1):
+            sum = i * A + (N - i) * B
+            if check(sum, C, D):
+                ans += fact[N] * binPower((fact[i] * fact[N - i]) % mod, mod - 2)
+                ans %= mod
+                
+        return int(ans)

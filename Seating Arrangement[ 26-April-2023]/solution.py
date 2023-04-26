8b077a0c310fef4b2410c17ec8a9125a06ba7da3
Bugs In Code <125942331+BugsInCodeYT@users.noreply.github.com>
2023-04-26 05:43:01 +0530

    Update solution.py

--- Seating Arrangement[ 26-April-2023]/solution.py
+++ Seating Arrangement[ 26-April-2023]/solution.py
@@ -1,5 +1,6 @@
 #    >> PYTHON CODE <<
 
+from typing import List
 
 class Solution:
     def is_possible_to_get_seats(self, n : int, m : int, seats : List[int]) -> bool:
@@ -16,15 +17,18 @@ class Solution:
                 if(i==0):
                     if(seats[i+1]!=1):
                         n-=1
+                        if(n==0): return True
                         seats[i]=1
                 elif i==m-1:
                     if(seats[i-1]!=1):
                         n-=1
+                        if(n==0): return True
                         seats[i]=1
                 else:
                     if(seats[i-1]!=1 and seats[i+1]!=1):
                         n-=1
+                        if(n==0): return True
                         seats[i]=1
             i+=1
 
-        return n<=0
+        return n==0

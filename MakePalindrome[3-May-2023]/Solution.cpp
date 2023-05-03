fc38ad50161b5f20311e66d5e3d78aa84dd6e935
Bugs In Code <125942331+BugsInCodeYT@users.noreply.github.com>
2023-05-03 19:52:23 +0530

    Update Solution.cpp

--- MakePalindrome[3-May-2023]/Solution.cpp
+++ MakePalindrome[3-May-2023]/Solution.cpp
@@ -1,46 +1,46 @@
-class Solution{
-public:
-    bool palindrome(string str)
+bool palindrome(string str)
+{
+    int low=0,high=str.length()-1;
+    
+    while(low<high)
     {
-        int low=0,high=str.length()-1;
-        
-        while(low<high)
-        {
-            if(str[low++]!=str[high--])
-            return false;
-        }
-        
-        return true;
+        if(str[low++]!=str[high--])
+        return false;
     }
     
-    bool makePalindrome(int n, vector<string>& arr) {
-        map<string, int> map;
-        
-        for (int i = 0; i < n; i++) {
+    return true;
+}
+
+bool makePalindrome(int n, string arr[]) {
+    map<string, int> map;
+    map<string,int> palins;
+    
+    for (int i = 0; i < n; i++) {
+        string str = arr[i];
+        map[str] = map[str] + 1;
+    }
+
+    int cnt=0;
+    for (int i = 0; i < n; i++) {
+        if(!palindrome(arr[i])){
             string str = arr[i];
-            map[str] = map[str] + 1;
-        }
+            string rev = string(str.rbegin(), str.rend());
     
-        string onePalin="";
-        for (int i = 0; i < n; i++) {
-            if(!palindrome(arr[i])){
-                string str = arr[i];
-                string rev = string(str.rbegin(), str.rend());
-        
-                if (map[str] != map[rev]) {
-                        return false;
-                }
-            }
-            else
-            {
-                if(onePalin=="")
-                     onePalin=arr[i];
-                if(onePalin!=arr[i])
-                       return false;
-                
+            if (map[str] != map[rev]) {
+                    return false;
             }
         }
+        else
+        {
+            palins[arr[i]]=palins[arr[i]]+1;
+        }
     
-        return true;
     }
-};
+    
+    for(auto freq:palins)
+    {
+        if(freq.second%2==1) cnt++; 
+    }
+        
+    return cnt<=1;
+}

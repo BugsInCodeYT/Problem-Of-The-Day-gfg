7faf922db75cc4e7183dc2feec127d5f92e14cac
Bugs In Code <125942331+BugsInCodeYT@users.noreply.github.com>
2023-05-03 19:31:53 +0530

    Update Solution.py

--- MakePalindrome[3-May-2023]/Solution.py
+++ MakePalindrome[3-May-2023]/Solution.py
@@ -1,15 +1,43 @@
-def makePalindrome(self,n, arr):
-        map = defaultdict(int)
-        
-        for i in range(n):
-            str = arr[i]
-            map[str] = map.get(str, 0) + 1
-        
-        for i in range(n):
-            str = arr[i]
-            rev = str[::-1]
-        
-            if map[str] != map[rev]:
+from typing import List
+from collections import defaultdict
+
+class Solution:
+    
+
+    def makePalindrome(self,n, arr):
+        def palindrome(str):
+            low=0
+            high=len(str)-1
+            
+            while(low<high):
+                if(str[low]!=str[high]):
                     return False
+                low+=1
+                high-=1
+            
+            return True
+            
+        mp = defaultdict(int)
         
+        for i in range(n):
+            str = arr[i]
+            if str in mp:
+                mp[str] += 1
+            else:
+                mp[str] = 1
+        
+        onePalin=""
+        for i in range(n):
+            if(not palindrome(arr[i])):
+                str = arr[i]
+                rev = str[::-1]
+        
+                if (mp[str] != mp[rev]):
+                        return False
+            else:
+                if(onePalin==""):
+                     onePalin=arr[i]
+                if(onePalin!=arr[i]):
+                       return False
+                
         return True

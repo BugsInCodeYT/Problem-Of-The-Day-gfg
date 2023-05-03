0ee18de5dbdb477d610fcb3a0caaf275c92971ae
Bugs In Code <125942331+BugsInCodeYT@users.noreply.github.com>
2023-05-03 20:02:11 +0530

    Update Solution.cpp

--- MakePalindrome[3-May-2023]/Solution.cpp
+++ MakePalindrome[3-May-2023]/Solution.cpp
@@ -1,46 +1,49 @@
-bool palindrome(string str)
-{
-    int low=0,high=str.length()-1;
-    
-    while(low<high)
+class Solution{
+public:
+    bool palindrome(string str)
     {
-        if(str[low++]!=str[high--])
-        return false;
-    }
-    
-    return true;
-}
-
-bool makePalindrome(int n, string arr[]) {
-    map<string, int> map;
-    map<string,int> palins;
-    
-    for (int i = 0; i < n; i++) {
-        string str = arr[i];
-        map[str] = map[str] + 1;
-    }
-
-    int cnt=0;
-    for (int i = 0; i < n; i++) {
-        if(!palindrome(arr[i])){
-            string str = arr[i];
-            string rev = string(str.rbegin(), str.rend());
-    
-            if (map[str] != map[rev]) {
-                    return false;
-            }
-        }
-        else
-        {
-            palins[arr[i]]=palins[arr[i]]+1;
-        }
-    
-    }
-    
-    for(auto freq:palins)
-    {
-        if(freq.second%2==1) cnt++; 
-    }
+        int low=0,high=str.length()-1;
         
-    return cnt<=1;
-}
+        while(low<high)
+        {
+            if(str[low++]!=str[high--])
+            return false;
+        }
+        
+        return true;
+    }
+    
+    bool makePalindrome(int n,vector<string>& arr) {
+        unordered_map<string, int> map;
+        unordered_map<string, int> palins;
+        
+        for (int i = 0; i < n; i++) {
+            string str = arr[i];
+            map[str] = map[str] + 1;
+        }
+    
+        int cnt=0;
+        for (int i = 0; i < n; i++) {
+            if(!palindrome(arr[i])){
+                string str = arr[i];
+                string rev = string(str.rbegin(), str.rend());
+        
+                if (map[str] != map[rev]) {
+                        return false;
+                }
+            }
+            else
+            {
+                palins[arr[i]]=palins[arr[i]]+1;
+            }
+        
+        }
+        
+        for(auto freq:palins)
+        {
+            if(freq.second%2==1) cnt++; 
+        }
+            
+        return cnt<=1;
+    }
+};

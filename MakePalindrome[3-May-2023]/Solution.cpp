1dd93aa14533b217e1226433c5200175d65db56b
Bugs In Code <125942331+BugsInCodeYT@users.noreply.github.com>
2023-05-03 19:22:29 +0530

    Update Solution.cpp

--- MakePalindrome[3-May-2023]/Solution.cpp
+++ MakePalindrome[3-May-2023]/Solution.cpp
@@ -1,19 +1,46 @@
-bool makePalindrome(int n,vector<string> &arr){
-        unordered_map<string, int> map;
-    
-        for (int i = 0; i < n; i++) {
-            string str = arr[i];
-            map[str] = map[str] + 1;
-        }
+class Solution{
+public:
+    bool palindrome(string str)
+    {
+        int low=0,high=str.length()-1;
         
-        for (int i = 0; i < n; i++) {
-            string str = arr[i];
-            string rev(str.rbegin(), str.rend());
-        
-            if (map[str] != map[rev]) {
-                    return false;
-            }
+        while(low<high)
+        {
+            if(str[low++]!=str[high--])
+            return false;
         }
         
         return true;
     }
+    
+    bool makePalindrome(int n, vector<string>& arr) {
+        map<string, int> map;
+        
+        for (int i = 0; i < n; i++) {
+            string str = arr[i];
+            map[str] = map[str] + 1;
+        }
+    
+        string onePalin="";
+        for (int i = 0; i < n; i++) {
+            if(!palindrome(arr[i])){
+                string str = arr[i];
+                string rev = string(str.rbegin(), str.rend());
+        
+                if (map[str] != map[rev]) {
+                        return false;
+                }
+            }
+            else
+            {
+                if(onePalin=="")
+                     onePalin=arr[i];
+                if(onePalin!=arr[i])
+                       return false;
+                
+            }
+        }
+    
+        return true;
+    }
+};

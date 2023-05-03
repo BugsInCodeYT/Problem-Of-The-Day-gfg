28951a0d3726e64d5830f853a19db22c21fe3edf
Bugs In Code <125942331+BugsInCodeYT@users.noreply.github.com>
2023-05-03 19:51:21 +0530

    Update Solution.java

--- MakePalindrome[3-May-2023]/Solution.java
+++ MakePalindrome[3-May-2023]/Solution.java
@@ -1,4 +1,4 @@
-public static boolean palindrome(String str)
+ public static boolean palindrome(String str)
     {
         int low=0,high=str.length()-1;
         
@@ -12,13 +12,14 @@ public static boolean palindrome(String str)
     }
     public static boolean makePalindrome(int n, String[] arr) {
         Map<String, Integer> map = new HashMap<>();
+        Map<String,Integer> palins=new HashMap<>();
         
         for (int i = 0; i < n; i++) {
             String str = arr[i];
             map.put(str, map.getOrDefault(str, 0) + 1);
         }
     
-        String onePalin="";
+        int cnt=0;
         for (int i = 0; i < n; i++) {
             if(!palindrome(arr[i])){
                 String str = arr[i];
@@ -30,13 +31,17 @@ public static boolean palindrome(String str)
             }
             else
             {
-                if(onePalin.equals(""))
-                     onePalin=arr[i];
-                if(!onePalin.equals(arr[i]))
-                       return false;
-                
+                palins.put(arr[i],palins.getOrDefault(arr[i],0)+1);
             }
+        
         }
+        
+        for(var freq:palins.values())
+        {
+            if(freq%2==1) cnt++; 
+        }
+        
+        
     
-        return true;
+        return cnt<=1;
     }

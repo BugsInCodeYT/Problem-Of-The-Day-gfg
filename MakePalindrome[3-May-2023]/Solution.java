7cb4197ba99bbb91f0effe2effe5decd67b65b94
Bugs In Code <125942331+BugsInCodeYT@users.noreply.github.com>
2023-05-03 18:59:22 +0530

    Update Solution.java

--- MakePalindrome[3-May-2023]/Solution.java
+++ MakePalindrome[3-May-2023]/Solution.java
@@ -1,17 +1,40 @@
-public static boolean makePalindrome(int n, String[] arr) {
+public static boolean palindrome(String str)
+    {
+        int low=0,high=str.length()-1;
+        
+        while(low<high)
+        {
+            if(str.charAt(low++)!=str.charAt(high--))
+            return false;
+        }
+        
+        return true;
+    }
+    public static boolean makePalindrome(int n, String[] arr) {
         Map<String, Integer> map = new HashMap<>();
-    
+        
         for (int i = 0; i < n; i++) {
             String str = arr[i];
             map.put(str, map.getOrDefault(str, 0) + 1);
         }
     
+        String onePalin="";
         for (int i = 0; i < n; i++) {
-            String str = arr[i];
-            String rev = new StringBuffer(str).reverse().toString();
-    
-            if (!map.get(str).equals(map.get(rev))) {
-                    return false;
+            if(!palindrome(arr[i])){
+                String str = arr[i];
+                String rev = new StringBuffer(str).reverse().toString();
+        
+                if (!map.get(str).equals(map.get(rev))) {
+                        return false;
+                }
+            }
+            else
+            {
+                if(onePalin.equals(""))
+                     onePalin=arr[i];
+                if(!onePalin.equals(arr[i]))
+                       return false;
+                
             }
         }
     

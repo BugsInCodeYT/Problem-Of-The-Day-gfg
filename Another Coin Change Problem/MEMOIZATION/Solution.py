11db8af623863695dd72dcc060da20e6b17af1a4
Bugs In Code <125942331+BugsInCodeYT@users.noreply.github.com>
2023-05-06 10:51:36 +0530

    Update Solution.py

--- Another Coin Change Problem/MEMOIZATION/Solution.py
+++ Another Coin Change Problem/MEMOIZATION/Solution.py
@@ -1,17 +1,16 @@
 //  >> PYTHON: MEMOIZATION
+from typing import List
 
 
 class Solution:
     def solve(self, N, K, target, coins, dp):
         if K == 0 and target == 0:
             return True
-        if K <= 0 or target <= 0:
-            return False
         if dp[K][target] != None:
             return dp[K][target]
         ans = False
         for i in range(N):
-            if(target>=coins[i]):
+            if(target>=coins[i] and K-1>=0):
                 ans |= self.solve(N, K - 1, target - coins[i], coins, dp)
         dp[K][target] = ans
         return ans
@@ -19,4 +18,4 @@ class Solution:
     def makeChanges(self, N, K, target, coins):
         dp = [[None for _ in range(target + 1)] for _ in range(K + 1)]
         return self.solve(N, K, target, coins, dp)  
-    
+ 

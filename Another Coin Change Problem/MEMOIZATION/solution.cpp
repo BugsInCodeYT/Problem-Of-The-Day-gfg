9ca3978090779189f8195b0c4c78206f8177d0bb
Bugs In Code <125942331+BugsInCodeYT@users.noreply.github.com>
2023-05-06 10:49:31 +0530

    Update solution.cpp

--- Another Coin Change Problem/MEMOIZATION/solution.cpp
+++ Another Coin Change Problem/MEMOIZATION/solution.cpp
@@ -1,24 +1,22 @@
 //  >> C++: MEMOIZATION <<
-
 class Solution {
-    public boolean solve(int N, int K, int target, List<Integer> coins, int[][] dp) {
-        if (K == 0 && target == 0) return true;
-        if (K <= 0 || target <= 0) return false;
-        if (dp[K][target] != -1) return dp[K][target] == 1;
-        boolean ans = false;
+  public:
+    bool solve(int N, int K, int target, vector<int> &coins, vector<vector<int>> &dp) {
+            
+        if (K == 0 and target == 0) return true;
+        
+        if (dp[K][target] != -1) return dp[K][target];
+        bool ans = false;
         for (int i = 0; i < N; i++) {
-		if(target>=coins[i])
+            if(target>=coins[i] and K-1>=0)
                 ans |= solve(N, K - 1, target - coins[i], coins, dp);
         }
-        dp[K][target] = ans ? 1 : 0;
-        return ans;
+        
+        return dp[K][target] = ans;
+        
     }
-
-    public boolean makeChanges(int N, int K, int target, List<Integer> coins) {
-        int[][] dp = new int[K + 1][target + 1];
-        for (int i = 0; i <= K; i++) {
-            Arrays.fill(dp[i], -1);
-        }
+    bool makeChanges(int N, int K, int target, vector<int> &coins) {
+        vector<vector<int>> dp(K+1, vector<int>(target+1, -1));
         return solve(N, K, target, coins, dp);
     }
-}
+};

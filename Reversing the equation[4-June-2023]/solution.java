739ae315a9ebb316ac3e43f4a7f0f14886129e4d
Bugs In Code <125942331+BugsInCodeYT@users.noreply.github.com>
2023-06-04 06:05:15 +0530

    Update solution.java

--- Reversing the equation[4-June-2023]/solution.java
+++ Reversing the equation[4-June-2023]/solution.java
@@ -3,27 +3,29 @@
 String reverseEqn(String s)
 {
     
-    int i = s.length() - 1;
-    StringBuilder ans = new StringBuilder();
-    
-    while (i >= 0) {
-        int num = (int)(s.charAt(i)-'0');
+   int i = s.length() - 1;
+        StringBuilder ans = new StringBuilder();
         
-        int j = i - 1;
-        while (j >= 0 && s.charAt(j)>='0' && s.charAt(j)<='9') {
-            num += (int)(s.charAt(j)-'0') * 10;
-            j--;
+        while (i >= 0) {
+            int num = (int)(s.charAt(i)-'0');
+            
+            int j = i - 1;
+            int pow=1;
+            while (j >= 0 && s.charAt(j)>='0' && s.charAt(j)<='9') {
+                num += (int)(s.charAt(j)-'0') * Math.pow(10,pow);
+                pow++;
+                j--;
+            }
+        
+            ans.append(Integer.toString(num));
+            
+            while (j >= 0 && (s.charAt(j) < '0' || s.charAt(j) > '9')) {
+                ans.append(s.charAt(j));
+                j--;
+            }
+            
+            i = j;
         }
-    
-        ans.append(Integer.toString(num));
         
-        while (j >= 0 && (s.charAt(j) < '0' || s.charAt(j) > '9')) {
-            ans.append(s.charAt(j));
-            j--;
-        }
-        
-        i = j;
-    }
-    
-    return ans.toString();
+        return ans.toString();
 }

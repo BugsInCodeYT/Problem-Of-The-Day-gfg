ebc018ef6ca398b5e0301f24167f265bf76d14da
Bugs In Code <125942331+BugsInCodeYT@users.noreply.github.com>
2023-06-04 06:08:33 +0530

    Update solution.cpp

--- Reversing the equation[4-June-2023]/solution.cpp
+++ Reversing the equation[4-June-2023]/solution.cpp
@@ -3,28 +3,30 @@
 string reverseEqn (string s)
 {
     int i = s.length() - 1;
-    string ans = "";
-    
-    while (i >= 0) {
+        string ans = "";
         
-        int num = s[i] - '0';
+        while (i >= 0) {
             
-        int j = i - 1;
-        while (j >= 0 && s[j] >= '0' && s[j] <= '9') {
-            num += (s[j] - '0') * 10;
-            j -= 1;
-        }
-    
-        ans += to_string(num);
-        while(j>=0 and (s[j]<'0' or s[j]>'9')){
-             ans+=s[j];
-             j -= 1;
+            int num = s[i] - '0';
+                
+            int j = i - 1;
+            int pw=1;
+            while (j >= 0 && s[j] >= '0' && s[j] <= '9') {
+                num += (s[j] - '0') * pow(10,pw);
+                pw++;
+                j -= 1;
+            }
+        
+            ans += to_string(num);
+            while(j>=0 and (s[j]<'0' or s[j]>'9')){
+                 ans+=s[j];
+                 j -= 1;
+            }
+            
+            i = j;
         }
         
-        i = j;
-    }
-    
-   
-    
-    return ans;
+       
+        
+        return ans;
 }

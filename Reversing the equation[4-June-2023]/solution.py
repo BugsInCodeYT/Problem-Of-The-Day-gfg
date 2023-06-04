8843adf67b0a960bfad1ed3cc4880523d7e75e21
Bugs In Code <125942331+BugsInCodeYT@users.noreply.github.com>
2023-06-04 06:10:06 +0530

    Update solution.py

--- Reversing the equation[4-June-2023]/solution.py
+++ Reversing the equation[4-June-2023]/solution.py
@@ -1,25 +1,28 @@
 #  >> PYTHON CODE <<
 
-def reverseEqn(self, s):
+class Solution:
+    def reverseEqn(self, s):
         
-    i=len(s)-1
-    ans=""
-    while(i>=0):
-        
-        num=int(s[i])
+        i=len(s)-1
+        ans=""
+        while(i>=0):
             
-        j=i-1
-        while(j>=0 and s[j]>='0' and s[j]<='9'):
-            num=num+int(s[j])*10
-            j-=1
-    
-    
-        ans=ans+str(num)
+            num=int(s[i])
+                
+            j=i-1
+            pw=1
+            while(j>=0 and s[j]>='0' and s[j]<='9'):
+                num=num+int(s[j])*(10**pw)
+                pw+=1
+                j-=1
         
-        while(j>=0 and (s[j]<'0' or s[j]>'9')):
-            ans+=s[j]
-            j-=1
+        
+            ans=ans+str(num)
             
-        i=j
-        
-    return ans
+            while(j>=0 and (s[j]<'0' or s[j]>'9')):
+                ans+=s[j]
+                j-=1
+                
+            i=j
+
+        return ans

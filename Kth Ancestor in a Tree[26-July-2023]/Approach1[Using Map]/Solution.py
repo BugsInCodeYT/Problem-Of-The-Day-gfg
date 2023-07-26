c4b3cf1668e5d947d72cb3b908b158c9c9ef0e26
Bugs In Code <125942331+BugsInCodeYT@users.noreply.github.com>
2023-07-26 06:09:43 +0530

    Update Solution.py

--- Kth Ancestor in a Tree[26-July-2023]/Approach1[Using Map]/Solution.py
+++ Kth Ancestor in a Tree[26-July-2023]/Approach1[Using Map]/Solution.py
@@ -4,28 +4,17 @@ def kthAncestor(root,k, node):
     def getParents(root,par,parent_map):
         if(not root): return
     
-        parent_map[root]=par
+        parent_map[root.data]=par
         
-        getParents(root.left,root,parent_map)
-        getParents(root.right,root,parent_map)
-    
-    def find(root,node):
-        if(not root): return None
-        if(root.data==node): return root
-        
-        left_search=find(root.left,node)
-        right_search=find(root.right,node)
-        
-        return right_search if not left_search else left_search
-            
+        getParents(root.left,root.data,parent_map)
+        getParents(root.right,root.data,parent_map)
     
     mp={}
     
     getParents(root,-1,mp)
-    nn=find(root,node)
-    
-    while(nn in mp and k>0):
-        nn=mp[nn]
+
+    while(node in mp and k>0):
+        node=mp[node]
         k-=1
     
-    return nn.data if nn in mp else -1
+    return node if node in mp else -1
